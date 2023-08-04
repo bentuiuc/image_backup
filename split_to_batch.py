124c58e4e6ec1e3a38575e30ee58c5f1aa730d91
Ben Tan <btan90@gmail.com>
2023-08-03 20:06:32 -0700

    saving progress

--- split_to_batch.py
+++ split_to_batch.py
@@ -1,9 +1,17 @@
 from pathlib import Path
 
-def split_to_batch(p: str = '..'):
-    pictures_location = Path(p)
-    pictures_filename = [f for f in pictures_location.iterdir()]
-    print(pictures_filename[0])
+def split_to_batch(p: str = '..', batch_size: int = 30):
+    files_location = Path(p)    # the files path
+    files_filename = [f for f in files_location.iterdir()] # list of file names
+    
+    # return if file count is not big enough
+    if len(files_filename) <= batch_size:
+        print(f'file count <= batch size, not batching required')
+        return
+    
+    batched_files = [files_filename[x:x+batch_size] for x in range(0, len(files_filename), batch_size)] # split the filename into batches
+    files_location.mkdir()
+
 
 if __name__ == "__main__":
     split_to_batch()
